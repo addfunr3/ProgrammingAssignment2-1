356c2c783d95c835af81022b2d55c9bd74bcb02f
addfunr3 <raddin3@gmail.com>
2015-02-23 00:31:50 +1100

    Final Submit
    The final submission, comments and sample (test) data included

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As inversion  can be a difficult process, there is a benefit to storing it 
+## rather than calculating it repeatedly. 
+## These functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix is used to:
+## 1. Set and Get a Matrix
+## 3. Set and Get the Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+## Sets the inverse to NULL.
 
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+## Sets the matrix (defined in the arguments) as y.
+
+	get <- function() x
+## Gets the matrix.
+
+	setinverse <- function(inverse) inv <<- inverse
+# Sets the inverse of the matrix.
+	getinverse <- function() inv
+#Returns the inverse of the matrix, if it exists.
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+#Creates a list based on the above outputs.
 }
 
-
-## Write a short comment describing this function
+## Searches for whether there is an inverse already stored (and displays it),
+## otherwise it calculates the inverse and stores it for potential retrieval later.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+## Finds getinverse(from the list) and stores as inv.
+
+	if(!is.null(inv)) {
+		message("getting cached data.")
+		return(inv)
+	}
+## Searches for whether there is an inverse already computed (!is.na) and 
+## displays it if it exists.
+
+	data <- x$get()
+## Sets the matrix from get as the object data.
+
+  inv <- solve(data)
+## Calculates the inverse of the matrix.
+
+	x$setinverse(inv)
+##Returns the inverse found in Setinverse
+
+	inv
+##Prints the inverse.
 }
+
+## Data I used for testing.
+## x = rbind(c(4, 3), c(3, 2))
+## m = makeCacheMatrix(x)
+## m$get()
+## cacheSolve(m)
